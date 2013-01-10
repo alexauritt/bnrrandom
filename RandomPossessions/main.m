86b4aba99fc26251b4b0ce5bc22c83062b8d91b7
Alex Auritt <alexauritt@yahoo.com>
2013-01-10 10:50:10 -0500

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Alexander Auritt on 1/10/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
