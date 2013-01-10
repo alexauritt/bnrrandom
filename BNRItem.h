c64dfc485f09ba96f852ca14e31d7f9c5f1659b3
Alex Auritt <alexauritt@yahoo.com>
2013-01-10 13:15:38 -0500

    about to test subclass

---
+++ BNRItem.h
@@ -0,0 +1,35 @@
+//
+//  BNRItem.h
+//  RandomPossessions
+//
+//  Created by Alexander Auritt on 1/10/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *itemName;
+    NSString *serialNumber;
+    int valueInDollars;
+    NSDate *dateCreated;
+}
+
++ (id)randomItem;
+
+- (id)initWithItemName:(NSString *)name
+        valueInDollars:(int)value
+          serialNumber:(NSString *)sNumber;
+
+- (void)setItemName: (NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)i;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+@end
--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -7,14 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
+        NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        [items insertObject:@"Zero" atIndex:0];
+        
+        BNRItem *p = [[BNRItem alloc] initWithItemName:@"Red Sofa" valueInDollars:100 serialNumber:@"A1B2C"];
+        NSLog(@"%@", p);
         
     }
     return 0;
